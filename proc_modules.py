db4ac32a9619a462647430af90bf2d71c9aa5971
Kartik Ayyer <kartik.ayyer@desy.de>
2021-11-19 18:37:24 +0100

    proc_modules: Only processing CELL_SEL cells. Reordered chunk division among ranks

--- proc_modules.py
+++ proc_modules.py
@@ -34,14 +34,15 @@ nproc = comm.Get_size()
 assert nproc % 16 == 0
 ranks_per_module = nproc // 16
 my_module = rank // ranks_per_module
+module_rank = rank % ranks_per_module
 
 # Open VDS file
 f_vds = h5py.File(PREFIX+'vds/r%.4d_proc.cxi' % args.run, 'r')
 dset_vds = f_vds['entry_1/instrument_1/detector_1/data']
 nevt = args.nframes if args.nframes >= 0 else dset_vds.shape[0]
-nchunks = nevt // CHUNK_SIZE
+num_chunks = nevt // CHUNK_SIZE
 if rank == 0:
-    print('Processing %d events with %d ranks per module' % (nevt, ranks_per_module))
+    print('Processing %d events with %d ranks per module in run %d' % (nevt, ranks_per_module, args.run))
 
 # Create output datasets
 f_out = h5py.File(PREFIX+'events/r%.4d_events.h5'%args.run, 'w', driver='mpio', comm=comm)
@@ -64,29 +65,38 @@ cshape = tuple(np.array((2*(cx.max() - cx.min()) + 1, 2*(cy.max()-cy.min()) + 1)
 cen = np.array(cshape) // 2
 corr = np.zeros((16,) + cshape)
 powder = np.zeros((16, 512*128))
+cell_mask = np.zeros(NCELLS, dtype='bool')
+cell_mask[CELL_SEL] = True
+
+my_num_chunks = int(np.ceil(num_chunks / ranks_per_module))
+my_chunk_start = module_rank*my_num_chunks
+my_chunk_end = np.clip((module_rank+1)*my_num_chunks, -1, num_chunks)
+my_chunks = np.arange(my_chunk_start, my_chunk_end)
 
 stime = time.time()
-for i, ind in enumerate(range(rank % ranks_per_module, nchunks, ranks_per_module)):
+for i, ind in enumerate(my_chunks):
     s = ind * CHUNK_SIZE
     e = (ind+1) * CHUNK_SIZE
-    chunk_cid = np.arange(s, e) % NCELLS
-    frames = dset_vds[s:e, my_module] * mask[chunk_cid, my_module]
+    chunk_cell_mask = cell_mask[np.arange(s, e) % NCELLS]
+    chunk_ind = np.arange(s, e)[chunk_cell_mask]
+    chunk_cid = chunk_ind % NCELLS
+    frames = dset_vds[chunk_ind, my_module, :, :] * mask[chunk_cid, my_module]
 
-    for j in range(CHUNK_SIZE):
+    for j, frame_ind in enumerate(chunk_ind):
         if np.any(np.isnan(frames[j])):
             continue
         phot = np.clip(np.round(frames[j]/ADU_PER_PHOTON-0.3), 0, None).astype('i4').ravel()
 
         ngpix = num_goodpix[chunk_cid[j], my_module].sum()
-        dset_p1[s+j, my_module] = (phot==1).sum() / ngpix
-        dset_p2[s+j, my_module] = (phot==2).sum() / ngpix
-        dset_imean[s+j, my_module] = phot.sum() / ngpix
+        dset_p1[frame_ind, my_module] = (phot==1).sum() / ngpix
+        dset_p2[frame_ind, my_module] = (phot==2).sum() / ngpix
+        dset_imean[frame_ind, my_module] = phot.sum() / ngpix
 
         accumulate_corr(phot, corr, my_module)
         powder[my_module] += phot
 
     if rank == 0:
-        sys.stderr.write('\rChunk %d/%d (%.3f Hz)' % (ind+1, nchunks, (ind+1)*CHUNK_SIZE/(time.time()-stime)))
+        sys.stderr.write('\rChunk %d/%d (%.3f Hz)' % (ind+1, my_num_chunks, (ind+1)*CHUNK_SIZE*ranks_per_module/(time.time()-stime)))
         sys.stderr.flush()
 if rank == 0:
     sys.stderr.write('\nFinished processing in %.3f s\n' % (time.time()-stime))
@@ -94,6 +104,9 @@ if rank == 0:
 
 f_out.close()
 f_vds.close()
+if rank == 0:
+    sys.stderr.write('Closed events file in %.3f s\n' % (time.time()-stime))
+    sys.stderr.flush()
 
 if rank == 0:
     red_powder = np.zeros_like(powder)
@@ -110,4 +123,5 @@ else:
     comm.Reduce(corr, None, root=0, op=MPI.SUM)
 
 if rank == 0:
-    print('DONE')
+    sys.stderr.write('DONE in %.3f s\n' % (time.time()-stime))
+    sys.stderr.flush()
