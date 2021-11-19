622e42975a58eed416ddb809e098aadc83a519e0
Kartik Ayyer <kartik.ayyer@desy.de>
2021-11-19 18:37:57 +0100

    proc_modules_array: Using 1 node gives better performance

--- slurm/proc_modules_array.sh
+++ slurm/proc_modules_array.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 #SBATCH --array=55
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --time=04:00:00
 #SBATCH --export=ALL
 #SBATCH -J proc_idi
 #SBATCH -o .%j.out
 #SBATCH -e .%j.out
-#SBATCH --partition=upex
+#SBATCH --partition=upex-beamtime
+#SBATCH --reservation=upex_002841
 
-####SBATCH --partition=upex-beamtime
-####SBATCH --reservation=upex_002841
+####SBATCH --partition=upex
 
 # Change the runs to process using the --array option on line 3
 
@@ -21,9 +21,9 @@ module load anaconda3
 source deactivate
 source activate /gpfs/exfel/exp/MID/202102/p002841/usr/env
 
-mask_file=/gpfs/exfel/exp/MID/202102/p002841/scratch/geom/mask_goodpix_cells_03.npy
-which mpirun
-which python
-which h5ls
-#mpirun -mca btl_tcp_if_include ib0 python ../proc_modules.py $SLURM_ARRAY_TASK_ID -n 10000
+mask_file=/gpfs/exfel/exp/MID/202102/p002841/scratch/geom/mask_goodpix_cells_04.npy
+#which mpirun
+#which python
+#which h5ls
+#mpirun -mca btl_tcp_if_include ib0 python ../proc_modules.py $SLURM_ARRAY_TASK_ID -m ${mask_file} -n 3520
 mpirun -mca btl_tcp_if_include ib0 python ../proc_modules.py $SLURM_ARRAY_TASK_ID -m ${mask_file}
