1c1b0267f527f694ab8e0188a8c397c9d6a623a6
Kartik Ayyer <kartik.ayyer@desy.de>
2021-11-21 16:13:48 +0100

    constants: Added RAW_ADU_PER_PHOTON

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 PREFIX = '/gpfs/exfel/exp/MID/202102/p002841/scratch/'
 NCELLS = 352
-CELL_SEL = slice(1,300,1)
+#CELL_SEL = slice(1,300,1)
+#CELL_SEL = slice(1,352,2)
+CELL_SEL = slice(1,351,1)
 ADU_PER_PHOTON = 8.
+RAW_ADU_PER_PHOTON = 98.
 CHUNK_SIZE = 16
